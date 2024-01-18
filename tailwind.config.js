4b777ee0eb8b45743419bcd26b76fedf15b9e10f
HugoHamonangan <naberius103@gmail.com>
2024-01-18 15:45:57 +0700

    new folders

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,18 @@
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class',
-
-  content: ["index.html"],
+  content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'xs': '400px',  
+        'sm': '640px',  
+        'md': '768px',  
+        'lg': '1024px', 
+        'xl': '1280px', 
+        'xxl': '1440px', 
+      }
+    },
   },
   plugins: [],
 }
