854e9649b82b6b6e106f08a895e3e43166516335
HugoHamonangan <naberius103@gmail.com>
2024-05-18 02:23:18 +0700

    make new change to the project

--- assets/js/real.js
+++ assets/js/real.js
@@ -1,146 +1,139 @@
-const scrollers = document.querySelectorAll(".scroller");
+const scrollers = document.querySelectorAll('.scroller');
 
 scrollers.forEach((scroller) => {
-    scroller.setAttribute("data-animated", true);
+  scroller.setAttribute('data-animated', true);
 
-    const list = scroller.querySelector(".tag-list");
-    const contentOfList = Array.from(list.children);
+  const list = scroller.querySelector('.tag-list');
+  const contentOfList = Array.from(list.children);
 
-    contentOfList.forEach((item) => {
-        const duplicatedItems = item.cloneNode(true);
+  contentOfList.forEach((item) => {
+    const duplicatedItems = item.cloneNode(true);
 
-        duplicatedItems.setAttribute("aria-hidden", true);
+    duplicatedItems.setAttribute('aria-hidden', true);
 
-        list.appendChild(duplicatedItems);
-    });
+    list.appendChild(duplicatedItems);
+  });
 });
 
 let tl1 = gsap.timeline();
 let tl2 = gsap.timeline();
 
-
-tl1.to("#logoIntro", {
-    duration: 1.3,
-    y:0,    
+tl1.to('#logoIntro', {
+  duration: 1.3,
+  y: 0,
 });
 
-tl1.to("#logoIntro", {
-    duration: .01,
-    className: 'hidden',
-    delay:.8, 
+tl1.to('#logoIntro', {
+  duration: 0.01,
+  className: 'hidden',
+  delay: 0.8,
 });
 
-tl1.to("#glitchLogo", {
-    duration: 1,     
-    className: '-=hidden' ,
-    
+tl1.to('#glitchLogo', {
+  duration: 1,
+  className: '-=hidden',
 });
 
-tl1.to("#glitchLogo", {
-    duration: .01,    
-    className: 'hidden' ,
+tl1.to('#glitchLogo', {
+  duration: 0.01,
+  className: 'hidden',
 });
 
-tl1.to("#logoIntro", {
-    duration: .01,
-    className: '-=hidden' ,  
+tl1.to('#logoIntro', {
+  duration: 0.01,
+  className: '-=hidden',
 });
 
-tl1.to("#logoIntro", {
-    duration: .6,
-    opacity: 0,
-    delay:1,        
+tl1.to('#logoIntro', {
+  duration: 0.6,
+  opacity: 0,
+  delay: 1,
 });
 
-tl1.to("#intro", {
-    duration: 2,    
-    opacity: 0,
-    delay: .4,
-    onComplete: function () {
-        gsap.set("#intro", { visibility: "hidden" });
-    }
+tl1.to('#intro', {
+  duration: 2,
+  opacity: 0,
+  delay: 0.4,
+  onComplete: function () {
+    gsap.set('#intro', { visibility: 'hidden' });
+  },
 });
 
-tl1.to("#realLogo", {
-    duration: 1,
-    y:0,
-    x:0,
-    top:0,
-    left:0,        
+tl1.to('#realLogo', {
+  duration: 1,
+  y: 0,
+  x: 0,
+  top: 0,
+  left: 0,
 });
-
-
-
-
-
 
 gsap.to('#test', {
-    background: "url('./whitebox.svg') 0rem 0rem / cover no-repeat",    
-    scrollTrigger: {
-        trigger: "#test",
-        scroller: "body",
-        scrub: .1,
-        end: 'top 20%',
-        start: 'top 79%',
-        // markers: true,        
-    },    
-    duration: 1,
+  background: "url('./whitebox.svg') 0rem 0rem / cover no-repeat",
+  scrollTrigger: {
+    trigger: '#test',
+    scroller: 'body',
+    scrub: 0.1,
+    end: 'top 20%',
+    start: 'top 79%',
+    // markers: true,
+  },
+  duration: 1,
 });
 
 gsap.to('#box', {
-    opacity: 1,
-    scrollTrigger: {
-        trigger: "#box",
-        scroller: "body",
-        scrub: .2,
-        end: 'bottom 40%',
-        start: 'top 69%',
-        // markers: true,
-        // pin: true,
-    },
-    duration: .7,
+  opacity: 1,
+  scrollTrigger: {
+    trigger: '#box',
+    scroller: 'body',
+    scrub: 0.2,
+    end: 'bottom 40%',
+    start: 'top 69%',
+    // markers: true,
+    // pin: true,
+  },
+  duration: 0.7,
 });
 
 gsap.to('#box2', {
-    opacity: 1,
-    scrollTrigger: {
-        trigger: "#box2",
-        scroller: "body",
-        scrub: .2,
-        end: 'bottom 40%',
-        start: 'top 69%',
-        // markers: true,
-        // pin: true,
-    },
-    duration: .7,
+  opacity: 1,
+  scrollTrigger: {
+    trigger: '#box2',
+    scroller: 'body',
+    scrub: 0.2,
+    end: 'bottom 40%',
+    start: 'top 69%',
+    // markers: true,
+    // pin: true,
+  },
+  duration: 0.7,
 });
 
 gsap.to('#box3', {
-    opacity: 1,
-    scrollTrigger: {
-        trigger: "#box3",
-        scroller: "body",
-        scrub: .2,
-        end: 'bottom 40%',
-        start: 'top 69%',
-        // markers: true,
-        // pin: true,
-    },
-    duration: .7,
+  opacity: 1,
+  scrollTrigger: {
+    trigger: '#box3',
+    scroller: 'body',
+    scrub: 0.2,
+    end: 'bottom 40%',
+    start: 'top 69%',
+    // markers: true,
+    // pin: true,
+  },
+  duration: 0.7,
 });
 
 gsap.to('#box4', {
-    opacity: 1,
-    scrollTrigger: {
-        trigger: "#box4",
-        scroller: "body",
-        scrub: .2,
-        end: 'bottom 40%',
-        start: 'top 69%',
-        // markers: true,
-        // pin: true,
-    },
-    duration: .7,
+  opacity: 1,
+  scrollTrigger: {
+    trigger: '#box4',
+    scroller: 'body',
+    scrub: 0.2,
+    end: 'bottom 40%',
+    start: 'top 69%',
+    // markers: true,
+    // pin: true,
+  },
+  duration: 0.7,
 });
 
 
